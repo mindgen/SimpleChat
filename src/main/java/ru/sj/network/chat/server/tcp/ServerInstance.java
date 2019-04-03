1722f041d9a9dcf97665e6b13934b8f5c709f174
Eugene Sinitsyn <eugene@parusnet.ru>
2019-04-03 23:49:23 +0500

    Move class inside

--- src/main/java/ru/sj/network/chat/server/tcp/ServerInstance.java
+++ src/main/java/ru/sj/network/chat/server/tcp/ServerInstance.java
@@ -35,129 +35,129 @@ public class ServerInstance extends ThreadsServer {
                 new SessionsManagerImpl(),
                 ByteBuffer.allocate(this.getBufferCapacity()));
     }
-}
 
-class TcpServerWorker extends ServerWorker {
+    private class TcpServerWorker extends ServerWorker {
 
-    private ServerSocketChannel mServerSocket;
-    private InetSocketAddress mAddress;
-    private SessionsManagerImpl mManager;
-    private ByteBuffer mBufer;
+        private ServerSocketChannel mServerSocket;
+        private InetSocketAddress mAddress;
+        private SessionsManagerImpl mManager;
+        private ByteBuffer mBufer;
 
-    public TcpServerWorker(InetSocketAddress address, SessionsManagerImpl manager, ByteBuffer buffer) {
-        mAddress = address;
-        mManager = manager;
-        mBufer = buffer;
-    }
-
-    public void run() {
-        try
-        {
-            configureServer();
-            doWork();
-        }
-        catch (IOException e)
-        {
-            // TO-DO
-            // Inform main thread about this
-        }
-    }
-
-    private void doWork() throws IOException {
-        Selector eventSelector = openSelector();
-        if (null == eventSelector) {
-            return;
+        public TcpServerWorker(InetSocketAddress address, SessionsManagerImpl manager, ByteBuffer buffer) {
+            mAddress = address;
+            mManager = manager;
+            mBufer = buffer;
         }
 
-        mServerSocket.register(eventSelector, mServerSocket.validOps());
-        while (eventSelector.select() > -1 && !this.IsTerminated()) {
-            for (SelectionKey currentEvent : eventSelector.selectedKeys()) {
-                if (!currentEvent.isValid()) continue;
-                if (currentEvent.isAcceptable()) {
-                    acceptConnection(currentEvent);
-                }
-                else if (currentEvent.isReadable()) {
-                    readData(currentEvent);
-                }
-                else if (currentEvent.isWritable()) {
-                    writeData(currentEvent);
-                }
+        public void run() {
+            try
+            {
+                configureServer();
+                doWork();
             }
-            eventSelector.selectedKeys().clear();
+            catch (IOException e)
+            {
+                // TO-DO
+                // Inform main thread about this
+            }
         }
 
-    }
+        private void doWork() throws IOException {
+            Selector eventSelector = openSelector();
+            if (null == eventSelector) {
+                return;
+            }
 
-    private void acceptConnection(SelectionKey key) throws IOException {
-        SocketChannel newChannel = ((ServerSocketChannel)key.channel()).accept();
-        configureSocket(newChannel);
-        SelectionKey regKey = newChannel.register(key.selector(), SelectionKey.OP_READ);
-        regKey.attach(mManager.openSession());
-    }
+            mServerSocket.register(eventSelector, mServerSocket.validOps());
+            while (eventSelector.select() > -1 && !this.IsTerminated()) {
+                for (SelectionKey currentEvent : eventSelector.selectedKeys()) {
+                    if (!currentEvent.isValid()) continue;
+                    if (currentEvent.isAcceptable()) {
+                        acceptConnection(currentEvent);
+                    }
+                    else if (currentEvent.isReadable()) {
+                        readData(currentEvent);
+                    }
+                    else if (currentEvent.isWritable()) {
+                        writeData(currentEvent);
+                    }
+                }
+                eventSelector.selectedKeys().clear();
+            }
 
-    private void readData(SelectionKey key) {
-        ISession curSession = (ISession)key.attachment();
-        SocketChannel curSocket = (SocketChannel)key.channel();
-
-        int bytesRead;
-        try {
-            do {
-                bytesRead = curSocket.read(this.getBuffer());
-            } while (bytesRead > 0);
         }
-        catch (IOException e)
+
+        private void acceptConnection(SelectionKey key) throws IOException {
+            SocketChannel newChannel = ((ServerSocketChannel)key.channel()).accept();
+            configureSocket(newChannel);
+            SelectionKey regKey = newChannel.register(key.selector(), SelectionKey.OP_READ);
+            regKey.attach(mManager.openSession());
+        }
+
+        private void readData(SelectionKey key) {
+            ISession curSession = (ISession)key.attachment();
+            SocketChannel curSocket = (SocketChannel)key.channel();
+
+            int bytesRead;
+            try {
+                do {
+                    bytesRead = curSocket.read(this.getBuffer());
+                } while (bytesRead > 0);
+            }
+            catch (IOException e)
+            {
+                closeChannel(key);
+                return;
+            }
+
+            this.getBuffer().flip();
+            curSession.readData(this.getBuffer());
+            this.getBuffer().clear();
+
+            if (bytesRead < 0) closeChannel(key);
+        }
+
+        private ByteBuffer getBuffer() { return mBufer; }
+
+        private void writeData(SelectionKey key) {
+
+        }
+
+        private Selector openSelector() throws IOException {
+            return Selector.open();
+        }
+
+        private void configureServer() throws IOException {
+            mServerSocket = ServerSocketChannel.open();
+            configureServerSocket(mServerSocket);
+            mServerSocket.bind(mAddress);
+        }
+
+        private void configureServerSocket(ServerSocketChannel sckChannel) throws IOException {
+            configureSocket(sckChannel);
+
+            sckChannel.socket().setOption(StandardSocketOptions.SO_REUSEADDR, true);
+            sckChannel.socket().setOption(StandardSocketOptions.SO_REUSEPORT, true);
+        }
+
+        private void configureSocket(AbstractSelectableChannel sck) throws IOException {
+            sck.configureBlocking(false);
+        }
+
+        private void closeChannel(SelectionKey key)  {
+            ISession curSession = (ISession)key.attachment();
+            curSession.close();
+            key.cancel();
+            try {
+                key.channel().close();
+            }
+            catch(IOException E) {}
+        }
+
+        @Override
+        public String getName()
         {
-            closeChannel(key);
-            return;
+            return "NoneBlockWrk";
         }
-
-        this.getBuffer().flip();
-        curSession.readData(this.getBuffer());
-        this.getBuffer().clear();
-
-        if (bytesRead < 0) closeChannel(key);
-    }
-
-    private ByteBuffer getBuffer() { return mBufer; }
-
-    private void writeData(SelectionKey key) {
-
-    }
-
-    private Selector openSelector() throws IOException {
-        return Selector.open();
-    }
-
-    private void configureServer() throws IOException {
-        mServerSocket = ServerSocketChannel.open();
-        configureServerSocket(mServerSocket);
-        mServerSocket.bind(mAddress);
-    }
-
-    private void configureServerSocket(ServerSocketChannel sckChannel) throws IOException {
-        configureSocket(sckChannel);
-
-        sckChannel.socket().setOption(StandardSocketOptions.SO_REUSEADDR, true);
-        sckChannel.socket().setOption(StandardSocketOptions.SO_REUSEPORT, true);
-    }
-
-    private void configureSocket(AbstractSelectableChannel sck) throws IOException {
-        sck.configureBlocking(false);
-    }
-
-    private void closeChannel(SelectionKey key)  {
-        ISession curSession = (ISession)key.attachment();
-        curSession.close();
-        key.cancel();
-        try {
-            key.channel().close();
-        }
-        catch(IOException E) {}
-    }
-
-    @Override
-    public String getName()
-    {
-        return "NoneBlockWrk";
     }
 }
